9d83b32c79829c995bca43075caa1dbe4798e837
Phat Nguyen <phatnguyentanit@gmail.com>
2022-09-23 11:46:32 -0500

    Added dummy data

--- property-management-service/src/main/resources/data.sql
+++ property-management-service/src/main/resources/data.sql
@@ -1,51 +1,36 @@
--- CATEGORY
-/*
-INSERT INTO category(name)
-VALUES
-    ('Computer'),
-    ('Mobile Phone'),
-    ('Laptop'),
-    ('Accessory');
-
-
 -- USERS
-INSERT INTO users(email, first_name, last_name, password)
+INSERT INTO users(email)
 VALUES
-    ('phat@gmail.com', 'Phat', 'Nguyen', '$2a$10$u5xPEnqTU3lcPPItCotEf./kp4k2zj4Q9Mnko5W4nBca4pvhLs7Zq'),
-    ('john@gmail.com', 'John', 'Smith', '$2a$10$u5xPEnqTU3lcPPItCotEf./kp4k2zj4Q9Mnko5W4nBca4pvhLs7Zq'),
-    ('david@gmail.com', 'David', 'Nguyen', '$2a$10$u5xPEnqTU3lcPPItCotEf./kp4k2zj4Q9Mnko5W4nBca4pvhLs7Zq'),
-    ('smith@gmail.com', 'Smith', 'Nguyen', '$2a$10$u5xPEnqTU3lcPPItCotEf./kp4k2zj4Q9Mnko5W4nBca4pvhLs7Zq'),
-    ('adam@gmail.com', 'Adam', 'John', '$2a$10$u5xPEnqTU3lcPPItCotEf./kp4k2zj4Q9Mnko5W4nBca4pvhLs7Zq'),
-    ('michael@gmail.com', 'Michael', 'Mike', '$2a$10$u5xPEnqTU3lcPPItCotEf./kp4k2zj4Q9Mnko5W4nBca4pvhLs7Zq');
+    ('phat@gmail.com'),
+    ('thiep@gmail.com'),
+    ('yen@gmail.com'),
+    ('phong@gmail.com'),
+    ('adam@gmail.com'),
+    ('michael@gmail.com');
 
--- ROLE
-INSERT INTO role(role)
-VALUES
-    ('ADMIN'),
-    ('USER');
 
-INSERT INTO users_roles(user_id, roles_id)
-VALUES
-    (1, 1),
-    (1, 2),
-    (2, 1),
-    (3, 1),
-    (4, 2),
-    (5, 2),
-    (5, 1),
-    (6, 2);
 
--- PRODUCT
-INSERT INTO product(name, price, rating, category_id, user_id)
+-- PROPERTY
+
+INSERT INTO property(price, num_of_room, property_type, home_type, overview, street, city, zip, listed, deleted, available_date, owner_id)
 VALUES
-    ('Product-name-1', 20, 4, 1, 1),
-    ('Product-name-2', 30, 5, 1, 1),
-    ('Product-name-3', 40, 6, 1, 1),
-    ('Product-name-4', 50, 4, 1, 2),
-    ('Product-name-5', 30.3, 1, 1, 2),
-    ('Product-name-6', 20, 4, 2, 3),
-    ('Product-name-7', 30, 5, 2, 3),
-    ('Product-name-8', 40, 6, 2, 4),
-    ('Product-name-9', 50, 4, 2, 5),
-    ('Product-name-10', 30.3, 1, 2, 6);
- */
+    (150000, 3, 'RENT', 'APARTMENT', 'The best apartment in the US', '900 North 12th Street', 'Fairfield', '56556', true, false, CURRENT_DATE, 1),
+    (200000, 2, 'BUY', 'CONDO', 'The best condo in the US', '111 North 11th Street', 'Fairfield', '56556', true, false, CURRENT_DATE, 1),
+    (250000, 2, 'RENT', 'APARTMENT', 'The best apartment in the US', '222 North 10th Street', 'Fairfield', '56556', true, false, CURRENT_DATE, 1),
+    (100000, 3, 'BUY', 'APARTMENT', 'The best apartment in the US', '333 North 9th Street', 'Fairfield', '56556', true, false, CURRENT_DATE, 2),
+    (350000, 3, 'RENT', 'APARTMENT', 'The best apartment in the US', '444 North 8th Street', 'Fairfield', '56556', true, false, CURRENT_DATE, 2);
+
+-- PICTURE
+INSERT INTO picture(path, property_id)
+VALUES
+    ('/picture1', 1),
+    ('/picture2', 1),
+    ('/picture3', 1),
+    ('/picture1', 2),
+    ('/picture2', 2),
+    ('/picture1', 3),
+    ('/picture2', 3),
+    ('/picture1', 3),
+    ('/picture2', 4),
+    ('/picture1', 4),
+    ('/picture2', 5);
