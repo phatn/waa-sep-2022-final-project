09f1e803f91ccee53b77dd785bcf2af9e87bde62
Henry Ha <106703135+henryhacode@users.noreply.github.com>
2022-09-24 00:31:56 -0500

    be: correct property_type in sample data
    There are only SELL and RENT types.

--- property-management-service/src/main/resources/data.sql
+++ property-management-service/src/main/resources/data.sql
@@ -15,9 +15,9 @@ VALUES
 INSERT INTO property(price, num_of_room, property_type, home_type, overview, street, city, zip_code, listed, deleted, available_date, owner_id)
 VALUES
     (150000, 3, 'RENT', 'APARTMENT', 'The best apartment in the US', '900 North 12th Street', 'Fairfield', '56556', true, false, CURRENT_DATE, 1),
-    (200000, 2, 'BUY', 'CONDO', 'The best condo in the US', '111 North 11th Street', 'Fairfield', '56556', true, false, CURRENT_DATE, 1),
+    (200000, 2, 'SELL', 'CONDO', 'The best condo in the US', '111 North 11th Street', 'Fairfield', '56556', true, false, CURRENT_DATE, 1),
     (250000, 2, 'RENT', 'APARTMENT', 'The best apartment in the US', '222 North 10th Street', 'Fairfield', '56556', true, false, CURRENT_DATE, 1),
-    (100000, 3, 'BUY', 'APARTMENT', 'The best apartment in the US', '333 North 9th Street', 'Fairfield', '56556', true, false, CURRENT_DATE, 2),
+    (100000, 3, 'SELL', 'APARTMENT', 'The best apartment in the US', '333 North 9th Street', 'Fairfield', '56556', true, false, CURRENT_DATE, 2),
     (350000, 3, 'RENT', 'APARTMENT', 'The best apartment in the US', '444 North 8th Street', 'Fairfield', '56556', true, false, CURRENT_DATE, 2);
 
 -- PICTURE
