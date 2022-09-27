d47d041286c2ffc1bfcde68e4399298c43180354
Phong Lu <lutaiphong@gmail.com>
2022-09-27 13:20:26 -0500

    - Fix search widget.

--- property-management-portal/src/components/SearchWidget/SearchWidget.js
+++ property-management-portal/src/components/SearchWidget/SearchWidget.js
@@ -1,6 +1,6 @@
 import './SearchWidget.scss';
 import { Button, Grid, TextField } from '@mui/material';
-import { useState } from "react";
+import { useState } from 'react';
 import { useNavigate } from 'react-router';
 
 const SearchWidget = () => {
@@ -12,7 +12,7 @@ const SearchWidget = () => {
     // location: street, city, zipcode
     const locationChanged = (e) => {
         let txt = e.target.value;
-        let arr = txt.split(",");
+        let arr = txt.split(',');
         if (arr.length > 0) {
             setStreet(arr[0].trim());
         }
@@ -22,51 +22,50 @@ const SearchWidget = () => {
         if (arr.length > 2) {
             setZipCode(arr[2].trim());
         }
-    }
+    };
 
     const locationKeyDowned = (e) => {
         if (e.keyCode === 13) {
             searchClicked(e);
         }
-    }
+    };
 
     const searchClicked = (e) => {
         e.preventDefault();
         const searchParams = new URLSearchParams();
-        if (street !== "") {
-            searchParams.append("street", street);
+        if (street !== '') {
+            searchParams.append('street', street);
         }
-        if (city !== "") {
-            searchParams.append("city", city);
+        if (city !== '') {
+            searchParams.append('city', city);
         }
-        if (zipCode !== "") {
-            searchParams.append("zipCode", zipCode);
+        if (zipCode !== '') {
+            searchParams.append('zipCode', zipCode);
         }
-        let url = "/property-list?" + searchParams.toString();
+        let url = '/property-list?' + searchParams.toString();
         nav(url);
-    }
+    };
 
     return (
         <div className="searchWidget">
             <div className="property-search-header">
                 <h3>Find Your Future Home</h3>
             </div>
-            <div className="container property-search-body">
-                <Grid container spacing={2}>
-                    <Grid item xs={12} md={10} sx={{ mb: 2, minWidth: 300 }}>
-                        <TextField fullWidth placeholder="Address, City, Zipcode"
-                            id="locationInput"
-                            variant="outlined"
-                            onChange={locationChanged}
-                            onKeyDown={locationKeyDowned}>
-                        </TextField>
-                    </Grid>
-                    <Grid item xs={2} md={2} sx={{ mb: 2, minWidth: 10 }}>
-                        <Button variant="contained" sx={{ mt: 2 }}
-                            onClick={searchClicked}>Search</Button>
-                    </Grid>
+            <Grid container padding={2}>
+                <Grid item xs={12} sx={{mb: 2, minWidth: 300}}>
+                    <TextField
+                        fullWidth
+                        placeholder="Address, City, Zipcode"
+                        id="locationInput"
+                        variant="outlined"
+                        onChange={locationChanged}
+                        onKeyDown={locationKeyDowned}>
+                    </TextField>
                 </Grid>
-            </div>
+                <Grid item xs={12} sx={{textAlign: 'center'}}>
+                    <Button variant="contained" onClick={searchClicked}>Search</Button>
+                </Grid>
+            </Grid>
         </div>
     );
 };
