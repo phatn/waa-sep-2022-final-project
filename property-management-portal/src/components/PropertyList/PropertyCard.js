04a7f71fb0ab101fbbd053624e0f82677017cbf8
Henry Ha <106703135+henryhacode@users.noreply.github.com>
2022-09-30 13:33:46 -0500

    fe: show property detail as a modal in property list

--- property-management-portal/src/components/PropertyList/PropertyCard.js
+++ property-management-portal/src/components/PropertyList/PropertyCard.js
@@ -3,7 +3,8 @@ import { styled } from '@mui/material/styles';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
 
 const ExpandMore = styled((props) => {
     const { expand, ...other } = props;
@@ -17,14 +18,13 @@ const ExpandMore = styled((props) => {
 }));
 
 export default function PropertyCard(props) {
-    // console.log(props);
+    console.log(props);
 
     const [expanded, setExpanded] = useState(false);
-    const nav = useNavigate();
+    const location = useLocation();
 
     const cardClicked = () => {
         console.log("Card clicked")
-        nav("/property-detail/" + props.id);
     }
 
     const favIconClicked = (e) => {
@@ -37,36 +37,40 @@ export default function PropertyCard(props) {
     }
 
     return (
-        <Card sx={{ minWidth: 300 }}>
-            <CardActionArea onClick={cardClicked}>
-                <CardMedia component="img"
-                    height="200"
-                    image={props.mainPicture} />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {props.formattedPrice} {props.numOfRoom} bds
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {props.location.street}, {props.location.city} {props.location.zipCode}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-            <CardActions disableSpacing>
-                <IconButton aria-label="Add to favorites" onClick={favIconClicked}>
-                    <FavoriteIcon />
-                </IconButton>
-                <ExpandMore expand={expanded} onClick={expandClicked}
-                    aria-expanded={expanded} aria-label="Show More">
-                    <ExpandMoreIcon />
-                </ExpandMore>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                    <Typography paragraph>
-                        {props.overview}
-                    </Typography>
-                </CardContent>
-            </Collapse>
-        </Card>
+        <div>
+            <Card sx={{ minWidth: 300 }}>
+                <Link key={props.id} to={`/property-detail/${props.id}`} state={{ background: location }}>
+                    <CardActionArea onClick={cardClicked}>
+                        <CardMedia component="img"
+                            height="200"
+                            image={props.mainPicture} />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {props.formattedPrice} {props.numOfRoom} bds
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {props.location.street}, {props.location.city} {props.location.zipCode}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Link>
+                <CardActions disableSpacing>
+                    <IconButton aria-label="Add to favorites" onClick={favIconClicked}>
+                        <FavoriteIcon />
+                    </IconButton>
+                    <ExpandMore expand={expanded} onClick={expandClicked}
+                        aria-expanded={expanded} aria-label="Show More">
+                        <ExpandMoreIcon />
+                    </ExpandMore>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <Typography paragraph>
+                            {props.overview}
+                        </Typography>
+                    </CardContent>
+                </Collapse>
+            </Card>
+        </div>
     )
 }
